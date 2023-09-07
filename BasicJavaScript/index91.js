1807b4fc5c4d2cc40691a21f17b4fcd69052c866
Symbat <shimbatoo@gmail.com>
2023-09-07 11:20:58 +0600

    task 91

---
+++ BasicJavaScript/index91.js
@@ -0,0 +1,24 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+     {
+      "artist": "Joel",
+      "title": "Piano ",
+      "release_year": 1977,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": false
+    }
+  ];
